bba0ac8a06f9b9443fd6fc4f9bf2101500774b9e
Nick <nstewart@truefit.io>
2017-03-08 17:08:17 -0500

    Remove extraneous function call

--- src/load_routes.js
+++ src/load_routes.js
@@ -8,7 +8,7 @@ const defineRoute = (verb, route, implementation) => ({verb, route, implementati
 const defineSubscribers = (subscribers, key) =>
   _.filter(subscribers, s => s.subscribe && s.subscribe(key));
 
-const defineRoutes = (prefix, key, schema, database, subscribers) => {
+const defineRoutes = (prefix, key, database, subscribers) => {
   const applicableSubscribers = defineSubscribers(subscribers, key);
   const handler = defineHandler(key, database, applicableSubscribers);
   const route = `${prefix ? `/${prefix}` : ''}/${key}`;
@@ -29,7 +29,7 @@ const defineRoutes = (prefix, key, schema, database, subscribers) => {
 export default config => {
   const subscribers = (config.subscribers || []).map(s => s.implementation);
   const routes = _.keys(config.database.models).map(key =>
-    defineRoutes(config.prefix, key, config.database.models[key], config.database, subscribers)
+    defineRoutes(config.prefix, key, config.database, subscribers)
   );
 
   return _.flattenDeep(routes);
