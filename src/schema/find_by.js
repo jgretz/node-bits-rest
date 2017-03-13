9a515f59f008ec813a891d21e90f8c658cced484
Joshua Mathianas <mathianasj@gmail.com>
2017-03-13 10:21:50 -0400

    add odata config and passing to underlying database as an odata query object

--- src/schema/find_by.js
+++ src/schema/find_by.js
@@ -2,4 +2,5 @@ export const findBy = (name, database) =>
 ({
   id: id => database.findById(name, id),
   query: query => database.find(name, query),
+  odataQuery: query => database.odataQuery(name, query),
 });
--- src/schema/get.js
+++ src/schema/get.js
@@ -1,13 +1,22 @@
 import {findId} from './find_id';
 import {findBy} from './find_by';
+import _ from 'lodash';
+import odata from 'odata-parser';
+import url from 'url';
 
-export const get = (name, database) => {
+export const get = (name, database, odataConfig) => {
   const find = findBy(name, database);
 
   return req => {
     const id = findId(req);
     if (id) {
       return find.id(id);
+    }
+
+    if (odataConfig.enabled || _.includes(odataConfig.schemas, name)) {
+      const urlQuery = decodeURI(url.parse(req.url).query).replace(/\+/g, ' ');
+      const odataQuery = (urlQuery && urlQuery !== 'null') ? odata.parse(urlQuery) : {};
+      return find.odataQuery(odataQuery || {});
     }
 
     return find.query(req.query || {});
