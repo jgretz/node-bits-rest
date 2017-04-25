ab7395ba5b8feb5169e9221c712b5ac13496c7f1
Joshua Mathianas <mathianasj@gmail.com>
2017-04-25 10:09:28 -0400

    remove unnecessary logic

--- src/schema/odata_query.js
+++ src/schema/odata_query.js
@@ -24,10 +24,6 @@ const oDataFuncMap = {
   tolower: node => ({func: 'tolower', args: node.args}),
 };
 
-const removeInner = value => {
-  return value.$col ? value.$col : value;
-};
-
 const oDataTypeMap = {
   eq: node => ({[parseNode(node.left)]: parseNode(node.right)}),
   ne: node => ({[parseNode(node.left)]: {ne: parseNode(node.right)}}),
@@ -35,8 +31,8 @@ const oDataTypeMap = {
   ge: node => ({[parseNode(node.left)]: {ge: parseNode(node.right)}}),
   lt: node => ({[parseNode(node.left)]: {lt: parseNode(node.right)}}),
   le: node => ({[parseNode(node.left)]: {le: parseNode(node.right)}}),
-  and: node => ({and: [removeInner(parseNode(node.left)), removeInner(parseNode(node.right))]}),
-  or: node => ({or: [removeInner(parseNode(node.left)), removeInner(parseNode(node.right))]}),
+  and: node => ({and: [parseNode(node.left), parseNode(node.right)]}),
+  or: node => ({or: [parseNode(node.left), parseNode(node.right)]}),
   functioncall: node => parseFunctionCall(node),
   property: node => node.name,
   literal: node => node.value,
@@ -57,16 +53,7 @@ const parseLeftFunctionCall = root => {
     throw new Error(`Unsupported function call - ${root.left.func}`);
   }
 
-  const compareNode = {
-    type: root.type,
-    left: {
-      type: 'literal',
-      value: '$col',
-    },
-    right: root.right,
-  };
-
-  return {leftFunc: map(root.left), compare: parseNode(compareNode)};
+  return {leftFunc: map(root.left), compare: {[root.type]: parseNode(root.right)}};
 };
 
 const parseNode = root => {
