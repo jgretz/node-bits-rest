960e871242be46fb719ba400edbedc2efa0d6dcc
Josh Gretz <jgretz@truefit.io>
2017-09-05 10:48:04 -0400

    look on the query as well

--- src/schema/find_id.js
+++ src/schema/find_id.js
@@ -1,6 +1,10 @@
 export const findId = req => {
   // technically they caller should put the id in the url, but we
   // can be a little forgiving
+  if (req.query && req.query.id) {
+    return req.query.id;
+  }
+
   if (req.params && req.params.id) {
     return req.params.id;
   }
